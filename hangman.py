3c783b3183c604a6118448f2e1a73a96f080f26e
cjdunteman <cdunteman@wisc.edu>
2019-11-02 14:08:22 -0500

    version 1.0.0 complete

--- hangman.py
+++ hangman.py
@@ -1,43 +1,131 @@
 # HANGMAN
 
-import random
-wordlist = 'words.txt'
+from string import ascii_lowercase
+from words import get_random_word
 
 
-def get_user_guess(guess):
-    if len(guess) != 1:
-        print("That is not a valid guess")
+def get_num_attempts():
+    """Get min num attempts"""
+    while True:
+        num_attempts = input(
+            'How many incorrect attempts do you want? [1-25] ')
+        try:
+            num_attempts = int(num_attempts)
+            if 1 <= num_attempts <= 25:
+                return num_attempts
+            else:
+                print('{0} is not between 1 and 25'.format(num_attempts))
+        except ValueError:
+            print('{0} is not an integer between 1 and 25'.format(
+                num_attempts))
+
+
+def get_min_word_length():
+    """Get min word length"""
+    while True:
+        min_word_length = input(
+            'What minimum word length do you want? [4-16] ')
+        try:
+            min_word_length = int(min_word_length)
+            if 4 <= min_word_length <= 16:
+                return min_word_length
+            else:
+                print('{0} is not between 4 and 16'.format(min_word_length))
+        except ValueError:
+            print('{0} is not an integer between 4 and 16'.format(
+                min_word_length))
+
+
+def get_display_word(word, idxs):
+    """Display word"""
+    if len(word) != len(idxs):
+        raise ValueError('Word length and indices length are not the same')
+    displayed_word = ''.join(
+        [letter if idxs[i] else '*' for i, letter in enumerate(word)])
+    return displayed_word.strip()
+
+
+def get_next_letter(remaining_letters):
+    """Get next guess"""
+    if len(remaining_letters) == 0:
+        raise ValueError('There are no remaining letters')
+    while True:
+        next_letter = input('Choose the next letter: ').lower()
+        if len(next_letter) != 1:
+            print('{0} is not a single character'.format(next_letter))
+        elif next_letter not in ascii_lowercase:
+            print('{0} is not a letter'.format(next_letter))
+        elif next_letter not in remaining_letters:
+            print('{0} has been guessed before'.format(next_letter))
+        else:
+            remaining_letters.remove(next_letter)
+            return next_letter
+
+
+def play_hangman():
+    """Start game"""
+    # Get attempts and word length
+    print('Starting a game of Hangman...')
+    attempts_remaining = get_num_attempts()
+    min_word_length = get_min_word_length()
+
+    # Randomly choose word
+    print('Selecting a word...')
+    word = get_random_word(min_word_length)
+    print()
+
+    # Game state vars
+    idxs = [letter not in ascii_lowercase for letter in word]
+    remaining_letters = set(ascii_lowercase)
+    wrong_letters = []
+    word_solved = False
+
+    # Main loop
+    while attempts_remaining > 0 and not word_solved:
+        # Print game state
+        print('Word: {0}'.format(get_display_word(word, idxs)))
+        print('Attempts Remaining: {0}'.format(attempts_remaining))
+        print('Previous Guesses: {0}'.format(' '.join(wrong_letters)))
+
+        # Get next guess
+        next_letter = get_next_letter(remaining_letters)
+
+        # Check if guessed letter is in the word
+        if next_letter in word:
+            # Correct guess
+            print('{0} is in the word!'.format(next_letter))
+
+            # Show guessed letter
+            for i in range(len(word)):
+                if word[i] == next_letter:
+                    idxs[i] = True
+        else:
+            # Incorrect guess
+            print('{0} is NOT in the word!'.format(next_letter))
+
+            # Decrement remaining attempts and maintain list of incorrect guesses
+            attempts_remaining -= 1
+            wrong_letters.append(next_letter)
+
+        # Is word solved?
+        if False not in idxs:
+            word_solved = True
+        print()
+
+    # Game over, reveal word
+    print('The word is {0}'.format(word))
+
+    # Win or loss?
+    if word_solved:
+        print('Congratulations! You won!')
     else:
-        pass
+        print('Try again next time!')
+
+    # Play again?
+    try_again = input('Would you like to try again? [y/Y] ')
+    return try_again.lower() == 'y'
 
 
-def get_word(min_word_length):
-    """Get a random word from the wordlist using no extra memory."""
-    num_words_processed = 0
-    curr_word = None
-    with open(wordlist, 'r') as f:
-        for word in f:
-            if '(' in word or ')' in word:
-                continue
-            word = word.strip().lower()
-            if len(word) < min_word_length:
-                continue
-            num_words_processed += 1
-            if random.randint(1, num_words_processed) == 1:
-                curr_word = word
-    return curr_word
-
-
-def check_guess(guess, word):
-    if guess in word:
-        print("Good guess")
-    else:
-        print("Bad guess")
-
-
-word = get_word(5)
-print(word)
-print("*" * len(word))
-
-guess = input("Guess a letter! ")
-check_guess(guess, word)
+if __name__ == '__main__':
+    while play_hangman():
+        print()
